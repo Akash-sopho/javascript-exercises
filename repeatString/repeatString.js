c784bc81c99139f65f80319c7a93a64580a44670
Akash Goyal <goyalakash391@gmail.com>
2018-06-09 01:11:49 +0530

    repeating strings given number of times

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-var repeatString = function() {
-
+var repeatString = function(str, num) {
+	var i;
+	var res = "";
+	for (i = 0;i < num;i++)
+		res = res + str;
+	return res;
 }
 
 module.exports = repeatString
