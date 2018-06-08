764dccf14e748eb4a271ba503ab36dd28e27dfa8
Akash Goyal <goyalakash391@gmail.com>
2018-06-09 01:06:39 +0530

    add all numbers

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,10 @@
-var sumAll = function() {
-
+var sumAll = function(addend1, addend2) {
+	var i;
+	var sum = 0;
+	for (i = addend1;i < addend2 + 1;i++) {
+		sum = sum + i;
+	}
+	return sum;
 }
 
 module.exports = sumAll
